41b7123eeb57b590e8415e9385f47ead192b4885
HZ_Tinye <121790809@qq.com>
2016-04-15 21:08:22 +0800

    test

---
+++ IndexOfTest.java
@@ -0,0 +1,14 @@
+package com.java.test;
+
+import java.io.IOException;
+
+import com.java.homework.IndexOf;
+
+public class IndexOfTest {
+
+	public static void main(String[] args) throws IOException {
+		
+		IndexOf.isInFile("full");
+
+	}
+}
